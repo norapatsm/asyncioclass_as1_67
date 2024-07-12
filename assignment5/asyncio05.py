8157601ca166c3b344bc49be4420e727e4e0bb4b
nor <norapat.sm@gmail.com>
2024-07-12 10:24:25 +0700

    add as 5

---
+++ assignment5/asyncio05.py
@@ -0,0 +1,24 @@
+from random import random
+import asyncio
+
+# coroutine 
+async def cook_food(food_name):
+    time_cook = 1 + random()
+    print(f'Microwave ({food_name}) : Cooking {time_cook} seconds')
+    await asyncio.sleep(time_cook)
+    print(f'Microwave ({food_name}) : Finished cooking')
+    return food_name, time_cook
+
+# main coroutine
+async def main():
+    tasks = [asyncio.create_task(cook_food('Rice')), 
+             asyncio.create_task(cook_food('Noodle')), 
+             asyncio.create_task(cook_food('Curry'))]
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    for task in done:
+        food_name, time_cook = task.result()
+        print(f'Completed task: 1\n - {food_name} is completed in {time_cook} seconds')
+    print(f'Uncompleted task: {len(pending)}')
+
+# begin asyncio program
+asyncio.run(main())
