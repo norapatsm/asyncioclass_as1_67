3aadf3dcc8a10d03be63165730ce186d5feae553
nor <norapat.sm@gmail.com>
2024-08-12 20:49:23 +0700

    add comment

--- assignment1/thread07.py
+++ assignment1/thread07.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:#สร้าง ThreadPoolExecutor กำหนดจำนวนสูงสุดThreads ที่ทำงานพร้อมกัน
         executor.map(thread_function, range(3))
