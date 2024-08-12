3442894c0e61a7b9f1e67214d96d7ee2d4bd0dc7
nor <norapat.sm@gmail.com>
2024-08-12 20:50:44 +0700

    add comment

--- assignment1/thread07.py
+++ assignment1/thread07.py
@@ -16,3 +16,8 @@ if __name__ == "__main__":
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:#สร้าง ThreadPoolExecutor กำหนดจำนวนสูงสุดThreads ที่ทำงานพร้อมกัน
         executor.map(thread_function, range(3))
+
+# โปรแกรมนี้จะสร้าง ThreadPoolExecutor ที่สามารถรัน Threads พร้อมกันได้สูงสุด 3 ตัว
+# ฟังก์ชัน thread_function จะถูกเรียกใช้พร้อมกันด้วยค่า 0, 1, และ 2 โดยแต่ละการเรียกจะรันใน Thread แยกกัน
+# โปรแกรมจะบันทึกข้อมูลว่าแต่ละ Thread เริ่มทำงานและเสร็จสิ้นการทำงานผ่าน logging
+# การทำงานทั้งหมดจะถูกจัดการโดย ThreadPoolExecutor ซึ่งทำให้การจัดการหลาย Threads
