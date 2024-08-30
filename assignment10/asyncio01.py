901ad790316d246c280a5b171f7eb5267021c9ea
nor <norapat.sm@gmail.com>
2024-08-30 09:54:19 +0700

    add finish time consume and producer

--- assignment10/asyncio01.py
+++ assignment10/asyncio01.py
@@ -1,10 +1,12 @@
 # example of using an asyncio queue
 from random import random
 import asyncio
+import time 
  
 # coroutine to generate work
 async def producer(queue):
     print('Producer: Running')
+    start_time = time.time()
     # generate work
     for i in range(10):
         # generate a value
@@ -16,11 +18,13 @@ async def producer(queue):
         await queue.put(value)
     # send an all done signal
     await queue.put(None)
-    print('Producer: Done')
+    end_time = time.time()  # Record the end time
+    print(f'Producer: Done in {end_time - start_time:.2f} seconds')
 
 # coroutine to consume work
 async def consumer(queue):
     print('Consumer: Running')
+    start_time = time.time()
     # consume work
     while True:
         # get a unit of work
@@ -31,7 +35,8 @@ async def consumer(queue):
         # report
         print(f'\t> Consumer got {item}')
     # all done
-    print('Consumer: Done')
+    end_time = time.time()  # Record the end time
+    print(f'Consumer: Done in {end_time - start_time:.2f} seconds')
 
 #entry point coroutine
 async def main():
